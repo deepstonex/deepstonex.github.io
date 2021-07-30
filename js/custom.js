9bef8da9e3de642debf9cc39c27594efb0925e1f
vibhanshujainiiitr <cs19b1027@iiitr.ac.in>
2021-07-30 23:45:52 +0530

    Some more changes

--- js/custom.js
+++ js/custom.js
@@ -432,3 +432,39 @@ $(function () {
 		resetCallback: function () { }
 	});
 });
+
+$(document).ready(function () {
+	$('.multiple-items').slick({
+		centerMode: true,
+		dots: true,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+});
+
